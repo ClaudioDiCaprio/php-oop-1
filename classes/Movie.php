189b6fd942c09519ecf22d887d75149ac89c0c8a
Claudio <claudiodicaprio89@gmail.com>
2022-01-28 17:50:22 +0100

    var_dump_construct_setted

--- classes/Movie.php
+++ classes/Movie.php
@@ -1,7 +1,30 @@
 <?php
-
 class Movie{
-    
+
+    public $production;
+    public $originalTitle;
+    public $productionCountry;
+    public $poductionYear;
+    public $mainChar;
+    public $secondChar;
+    public $movieDirector;
+    public $script;
+
+    //CONSTRUCTOR-START//
+
+    public function __construct($_production, $_originalTitle, $_mainChar, $_secondChar,  $_productionCountry, $_poductionYear, $_movieDirector, $_script)
+    {   
+        $this->production = $_production;
+        $this->originalTitle = $_originalTitle;
+        $this->mainChar = $_mainChar;
+        $this->secondChar = $_secondChar;
+        $this->productionCountry = $_productionCountry;
+        $this->poductionYear = $_poductionYear;
+        $this->movieDirector = $_movieDirector;
+        $this->script = $_script;
+    }
+
+    //CONSTRUCTOR-END//
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 require_once __DIR__ .'/classes/Movie.php';
 
+$movie_1 = new Movie("Universal Pictures"," Back to the Future"," Michael J. Fox "," Christopher lloyd"," USA"," 1985"," Robert Zemeckis"," Robert Zemeckis");
+
+$movie_2 = new Movie("Universal Pictures"," Back to the Future II"," Michael J. Fox "," Christopher lloyd"," USA"," 1989"," Bob Gale"," Robert Zemeckis/Bob Gale");
+
+$movie_3 = new Movie("Universal Pictures"," Back to the Future III"," Michael J. Fox "," Christopher lloyd"," USA"," 1990"," Robert Zemeckis"," Bob Gale");
+
+var_dump($movie_1,$movie_2,$movie_3);
 ?>
