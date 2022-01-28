b44c301bf4356047821c6c1870f1b6483fcd1085
Claudio <claudiodicaprio89@gmail.com>
2022-01-28 17:54:09 +0100

    as_array

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
 require_once __DIR__ .'/classes/Movie.php';
 
-$movie_1 = new Movie("Universal Pictures"," Back to the Future"," Michael J. Fox "," Christopher lloyd"," USA"," 1985"," Robert Zemeckis"," Robert Zemeckis");
+// $movie_1 = new Movie("Universal Pictures"," Back to the Future"," Michael J. Fox "," Christopher lloyd"," USA"," 1985"," Robert Zemeckis"," Robert Zemeckis");
 
-$movie_2 = new Movie("Universal Pictures"," Back to the Future II"," Michael J. Fox "," Christopher lloyd"," USA"," 1989"," Bob Gale"," Robert Zemeckis/Bob Gale");
+// $movie_2 = new Movie("Universal Pictures"," Back to the Future II"," Michael J. Fox "," Christopher lloyd"," USA"," 1989"," Bob Gale"," Robert Zemeckis/Bob Gale");
 
-$movie_3 = new Movie("Universal Pictures"," Back to the Future III"," Michael J. Fox "," Christopher lloyd"," USA"," 1990"," Robert Zemeckis"," Bob Gale");
+// $movie_3 = new Movie("Universal Pictures"," Back to the Future III"," Michael J. Fox "," Christopher lloyd"," USA"," 1990"," Robert Zemeckis"," Bob Gale");
 
-var_dump($movie_1,$movie_2,$movie_3);
+// var_dump($movie_1,$movie_2,$movie_3);
+
+//ANZICHÉ DEFINIRLI INDIVIDUALMENTE,COME FATTO SOPRA, LI SPRIGIONIAMO COME UN ARRAY
+
+$movie =[
+    new Movie("Universal Pictures"," Back to the Future"," Michael J. Fox "," Christopher lloyd"," USA"," 1985"," Robert Zemeckis"," Robert Zemeckis"),
+    new Movie("Universal Pictures"," Back to the Future II"," Michael J. Fox "," Christopher lloyd"," USA"," 1989"," Bob Gale"," Robert Zemeckis/Bob Gale"),
+    new Movie("Universal Pictures"," Back to the Future III"," Michael J. Fox "," Christopher lloyd"," USA"," 1990"," Robert Zemeckis"," Bob Gale")
+];
+
 ?>
